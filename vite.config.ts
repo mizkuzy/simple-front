c352b30cb9384840a5429cfe35945326067a2b24
mizkuzy <mizkuzy@yandex.ru>
2025-12-28 21:47:58 +0200

    dockerize

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,13 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  server: {
+    host: true, // or '0.0.0.0'
+    port: 5173,
+    watch: {
+      usePolling: true // needed for file changes in Docker
+    }
+  },
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
